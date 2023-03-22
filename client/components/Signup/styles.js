5084d59f89b2d889c97a3fe6ecbaa0283f61fe88
tryst6969 <trystanretirado@gmail.com>
2023-03-22 15:40:52 +0800

    UI for login and Reg

---
+++ client/components/Signup/styles.js
@@ -0,0 +1,79 @@
+import { ScaledSheet } from 'react-native-size-matters';
+import colors from '../../assets/themes/colors';
+
+export default styles = ScaledSheet.create({
+    container: {
+        margin: 20
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10
+    },
+    error: {
+        color: 'red',
+        fontSize: 16,
+        paddingLeft:'10@s',
+    },
+
+    signupLink: {
+        paddingLeft:'5@s',
+        color:colors.blue,
+        width:'140@s',
+        alignSelf:'auto',
+    },
+
+    logoImage:{
+        height: '150@s',
+        width: '600@s',
+        marginTop:'30@s',
+        marginBottom: '100@s',
+        alignSelf:'center',
+        
+    },
+    loginImage:{
+        height: '650@s',
+        width: '350@s',
+        flex:1,
+        alignSelf:'center',
+    },
+    subText:{
+        marginTop: '20@s',
+        marginBottom:'1@s',
+        fontSize:'17@s',
+        textAlign:'center',
+        paddingVertical: '10@s',
+        fontWeight: 'bold',
+        fontSize:'30@s',     
+    },
+    loginText:{
+        marginTop: '10@s',
+        width:'290@s',
+        alignSelf:'center', 
+    },
+    passText:{
+        marginTop: '-20@s',
+        width:'290@s',
+        alignSelf:'center',
+    },
+    wrapper: {
+        height:'42@s',
+        width:'300@s',
+        paddingHorizontal:'5@s',
+        marginVertical: '5@s',
+        borderRadius:'10@s',
+        alignItems:'center',
+        justifyContent:'space-evenly',
+        alignSelf:'center',
+        marginTop:'1@s',
+    },
+    buttonText:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: colors.white
+    }
+})
--- client/screens/Signup.js
+++ client/screens/Signup.js
@@ -1,58 +1,13 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import { useSignup } from '../hooks/useSignup'
+import { View, Text } from 'react-native'
+import React from 'react'
+import SignupComp from '../components/Signup/signupComponent'
 
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const {signup, error, isLoading} = useSignup()
-
-  const handleSubmit = async () => {
-    await signup(email, password)
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Sign Up</Text>
-
-      <Text>Email address:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
+    <SignupComp>
       
-      <Text>Password:</Text>
-      <TextInput
-        style={styles.input}
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-      />
+    </SignupComp>
+  )
+}
 
-      <Button disabled={isLoading} title="Sign up" onPress={handleSubmit} />
-      {error && <Text style={styles.error}>{error}</Text>}
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        margin: 20
-    },
-    header: {
-        fontSize: 24,
-        fontWeight: 'bold'
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 10
-    },
-    error: {
-        color: 'red',
-        fontSize: 16
-    }
-})
-
-export default Signup;
+export default Signup
