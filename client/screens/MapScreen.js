4c6049f76dde1a05d84f7714dd2bc5ca2c230d11
tryst6969 <trystanretirado@gmail.com>
2023-03-23 19:58:24 +0800

    fixed broken scroll

--- client/screens/MapScreen.js
+++ client/screens/MapScreen.js
@@ -4,6 +4,8 @@ import MapComponent from '../components/MapComponent/MapComponent'
 import BottomNavComp from '../components/BottomSearchNav/BottomMapSearchNav'
 import { usePreferencesContext } from '../hooks/usePreferencesContext'
 import { useAuthContext } from '../hooks/useAuthContext'
+import ExtraNavigation from '../components/BottomSearchNav/Extra/ExtraNav'
+
 
 const MapScreen = () => {
   const [loading, setLoading] = useState(true)
@@ -39,6 +41,8 @@ const MapScreen = () => {
         <View style={{ height: 700 }}>
           <MapComponent />
           <BottomNavComp preference={preferences} />
+          
+          
         </View>
       )}
     </View>
