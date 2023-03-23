455502833d061d5cb35d6a1a1b504f9b2eba06c6
franklinnazareno <franklinnazareno@gmail.com>
2023-03-23 11:15:29 +0800

    Finding path passes user preferences, and source and destination coords

--- server/models/preferenceModel.js
+++ server/models/preferenceModel.js
@@ -6,15 +6,17 @@ const preferenceSchema = new Schema({
     preferences: {
         type: [{
             name: { type: String, required: true },
+            display: { type: String, required: true },
             value: { type: Number, required: true, default: 5 },
             enabled: { type: Boolean, required: true, default: true }
         }],
         default: [
-            { name: 'cameras', value: 5, enabled: true },
-            { name: 'street-lightings', value: 5, enabled: true },
-            { name: 'landmarks', value: 5, enabled: true },
-            { name: 'pwd', value: 5, enabled: true },
-            { name: 'major-roads', value: 5, enabled: true }
+            { name: 'not_flood_hazard', display: 'Avoid Flood-prone Areas', value: 5, enabled: true },
+            { name: 'pwd_friendly', display: 'PWD-friendly', value: 5, enabled: true },
+            { name: 'cctv', display: 'CCTV-presence', value: 5, enabled: true },
+            { name: 'landmark', display: 'Landmarks', value: 5, enabled: true },
+            { name: 'lighting', display: 'Well-lit Areas', value: 5, enabled: true },
+            { name: 'not_major_road', display: 'Avoid Major Roads', value: 5, enabled: true }
         ]
     },
     user_id: {
