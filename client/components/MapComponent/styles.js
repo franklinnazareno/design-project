42a3c154b56654d3ff308c8bd3bd2967826678e8
tryst6969 <trystanretirado@gmail.com>
2023-03-29 20:21:08 +0800

    Map pinching fix

---
+++ client/components/MapComponent/styles.js
@@ -0,0 +1,11 @@
+import { ScaledSheet } from 'react-native-size-matters';
+import colors from '../../assets/themes/colors';
+
+
+
+export default ScaledSheet.create ({
+    Mapsize:{
+        height:'600@s',
+        flex:'1@s'
+    }
+})
