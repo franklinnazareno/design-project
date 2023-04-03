7a21b4a03d62c56d3a6dc498e0f2106f5814f2fc
franklinnazareno <franklinnazareno@gmail.com>
2023-04-03 22:18:07 +0800

    Get current location button now works. Report is still broken though

--- client/components/reportComp/reportComponent.js
+++ client/components/reportComp/reportComponent.js
@@ -1,7 +1,10 @@
-import { View, Text, SafeAreaView, TextInput, TouchableOpacity, Button, ImageBackground } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity, Button, ImageBackground, Image } from 'react-native'
+import React, { useState } from 'react'
 import Entypo from 'react-native-vector-icons/Entypo'
+import {launchImageLibrary} from 'react-native-image-picker';
 
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useLogout } from '../../hooks/useLogout';
 import Input from '../inputs'
 import SecondaryInput from '../commons/secondaryInput'
 import styles from './styles'
@@ -9,6 +12,73 @@ import CustomButton from '../CustomButton'
 
 
 const ReportComponent = () => {
+  const [source, setSource] = useState('')
+  const [description, setDescription] = useState('')
+  const [image, setImage] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const { user } = useAuthContext()
+  const { logout } = useLogout()
+
+  const handleImageUpload = () => {
+  const options = {
+    mediaType: 'photo',
+    selectionLimit: 1,
+  }
+  launchImageLibrary(options, (response) => {
+    console.log('Image response:', response)
+    if (response?.didCancel) {
+      console.log('Image selection cancelled')
+    } else if (response?.error) {
+      console.log('Image selection error:', response.error)
+      setError(response.error)
+    } else {
+      setImage(response)
+    }
+  })
+}
+
+  const handleSubmit = async () => {
+    setLoading(true)
+    try {
+      const formData = new FormData()
+      formData.append('Source', source)
+      formData.append('Description', description)
+      if (image) {
+        formData.append('Image', {
+          uri: image.uri,
+          name: image.fileName,
+          type: image.type
+        })
+      }
+
+      const response = await fetch('http://10.0.2.2:4000/api/report', {
+        method: 'POST',
+        headers: {
+          'Accept': '*/*',
+          'Content-Type': 'multipart/form-data',
+          'Authorizaton': `Bearer ${user.token}`,
+        },
+        body: formData
+      })
+
+      const responseData = await response.json()
+
+      if (response.ok) {
+        console.log(responseData)
+      }
+      if (!response.ok) {
+        logout()
+        return <Login />
+      }
+      setLoading(false)
+    } catch (error) {
+      setError(error)
+      setLoading(false)
+    }
+  }
+
   return (
     <SafeAreaView>
         <Text style={styles.subText}> Got a Problem? Report Now</Text>
@@ -21,6 +91,8 @@ const ReportComponent = () => {
             
         <Input
             label='Source'
+            value={source}
+            onChangeText={setSource}
             icon={<TouchableOpacity >
             <Entypo name = 'location' size={25}></Entypo>
             </TouchableOpacity>}
@@ -29,7 +101,9 @@ const ReportComponent = () => {
 
         <View>
         <SecondaryInput
-        label='Report Problem'
+        label='Description'
+        value={description}
+        onChangeText={setDescription}
         icon={<TouchableOpacity >
           <Entypo name = 'location' size={25}></Entypo>
           </TouchableOpacity>}
@@ -38,11 +112,20 @@ const ReportComponent = () => {
         </View>
 
         <View style={styles.ImageUpload}>
-        <TouchableOpacity style={styles.saveButton}>
+        <TouchableOpacity style={styles.saveButton} onPress={handleImageUpload}>
         <Text style={styles.saveButtonText}>Upload Image</Text>
         </TouchableOpacity>
         </View>
-        <CustomButton primary title='Report'></CustomButton>
+        {image && (
+          <View style={styles.imagePreview}>
+            {image.uri && (
+              <Image source={{ uri: image.uri }} style={styles.image} />
+            )}
+            <Text style={styles.imageName}>{image.fileName}</Text>
+          </View>
+        )}
+        <CustomButton disabled={loading} primary title='Report' onPress={handleSubmit}/>
+        {/* {error && <Text style={styles.error}>{error}</Text>} */}
         </View>
         
     
