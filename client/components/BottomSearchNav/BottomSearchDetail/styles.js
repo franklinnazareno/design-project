990bcf8dec6061f44f7b08d8e5ff7b9dd7db46dc
Kristian Baturiano <kjqb4929@gmail.com>
2023-04-10 20:33:52 +0800

    UI changes

--- client/components/BottomSearchNav/BottomSearchDetail/styles.js
+++ client/components/BottomSearchNav/BottomSearchDetail/styles.js
@@ -151,9 +151,9 @@ export default ScaledSheet.create ({
     alignItems:'center'
   },
   labeltext: {
-    padding: '2@s', 
+    padding: '5@s', 
     fontWeight:'bold', 
-    fontSize: '25@s',
+    fontSize: '20@s',
     color: colors.primary
   }
 
