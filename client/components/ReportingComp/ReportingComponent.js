b43a44a178495acd61a2c7c576afa5d761f9d445
danangeloj <danangelojulongbayan@gmail.com>
2023-04-28 13:25:39 +0800

    report component image

--- client/components/ReportingComp/ReportingComponent.js
+++ client/components/ReportingComp/ReportingComponent.js
@@ -1,7 +1,8 @@
-import { View, Text, TouchableOpacity, ImageBackground, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, ImageBackground, ScrollView, Image } from 'react-native'
 import React, { useState, useEffect, useRef } from 'react'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Config from 'react-native-config';
+import {launchImageLibrary} from 'react-native-image-picker';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import Input from '../commons/inputs'
 import SecondaryInput from '../commons/secondaryInput'
@@ -9,16 +10,16 @@ import CustomButton from '../commons/CustomButton'
 import MapContainer from '../commons/mapContainer/Contain';
 import styles from './styles';
 
-navigator.geolocation = require('@react-native-community/geolocation');
-
 const ReportingComponent = ({ location }) => {
   const [source, setSource] = useState('')
   const [description, setDescription] = useState('')
+  const [image, setImage] = useState(null)
   const [error, setError] = useState(null)
   const [success, setSuccess] = useState(false)
   const [loading, setLoading] = useState(false)
 
   const { user } = useAuthContext()
+
 
   useEffect(() => {
     async function getLocation() {
@@ -39,6 +40,26 @@ const ReportingComponent = ({ location }) => {
     getLocation();
   }, []);
 
+  const handleImageUpload = () => {
+    const options = {
+      mediaType: 'photo',
+      selectionLimit: 1,
+    }
+    launchImageLibrary(options, (response) => {
+      console.log('Image response:', response)
+      if (response?.didCancel) {
+        console.log('Image selection cancelled')
+      } else if (response?.error) {
+        console.log('Image selection error:', response.error)
+        setError(response.error)
+      } else {
+        console.log('uri -> ', response.assets[0].uri);
+        console.log('fileSize -> ', response.assets[0].fileSize);
+        setImage(response.assets[0])
+      }
+    })
+  }
+
   const handleLocation = async () => {
     if (location) {
         const longitude = location.longitude
@@ -57,20 +78,23 @@ const ReportingComponent = ({ location }) => {
   const handleSubmit = async () => {
     setLoading(true)
     try {
-      const requestBody = {
-        Source: source,
-        Description: description
-    }
-
-    console.log(requestBody)
+      const formData = new FormData()
+      formData.append('source', source)
+      formData.append('description', description)
+      formData.append('image', {
+        name: image.fileName,
+        type: image.type,
+        uri: image.uri
+      })
 
       const response = await fetch(`${Config.EXPRESS}/api/report`, {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${user.token}`,
-          'Content-Type': 'application/json',
+          'Content-Type': 'multipart/form-data',
+          'Accept':'*/*'
         },
-        body: JSON.stringify(requestBody)
+        body: formData
       })
 
       const responseData = await response.json()
@@ -116,7 +140,21 @@ const ReportingComponent = ({ location }) => {
         onChangeText={setDescription}
         />
         </View>
-        <CustomButton disabled={loading} primary title='Report' onPress={handleSubmit}/>
+        <View style={styles.Imageupload}>
+        <TouchableOpacity style={styles.saveButton} onPress={handleImageUpload}>
+        <Text style={styles.saveButtonText}>Upload Image</Text>
+        </TouchableOpacity>
+        </View>
+        {image && (
+          <View style={styles.imagePreview}>
+            {image.uri && (
+              <Image source={{ uri: image.uri }} style={styles.image} />
+            )}
+            <Text style={styles.imageName}>Image uploaded successfully</Text>
+          </View>
+        )}
+        {source && description && image ? <CustomButton disabled={loading} primary title='Report' onPress={handleSubmit}/> 
+        : <CustomButton disabled primary title='Report'/>}
         {error && <Text style={styles.error}>Error: {error.message}</Text>}
         {success && <Text style={styles.success}>Report sent successfully!</Text>}
         </View>
