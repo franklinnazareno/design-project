86f04508b231cee5582d18952da99b00a7877c6c
franklinnazareno <franklinnazareno@gmail.com>
2023-04-30 14:44:02 +0800

    enhanced signup error handling and report feature

--- server/controllers/reportController.js
+++ server/controllers/reportController.js
@@ -2,12 +2,26 @@ const Report = require('../models/reportModel')
 
 // create new report
 const createReport = async (req, res) => {
-    const { Source, Description } = req.body;
+    const { source, description } = req.body;
+
+    if (!req.file) {
+        return res.status(400).json({ error: "No image file was uploaded" })
+    }
+
+    if (!req.file.mimetype.startsWith("image/")) {
+        return res.status(400).json({ error: "Uploaded file is not a valid image" })
+    }
+
+    if (req.file.size > 20 * 1024 * 1024) {
+        return res.status(400).json({ error: "Uploaded file exceeds the maximum size of 20 MB" });
+    }
+
+    const image = req.file.buffer
 
     try {
         const user_id = req.user._id;
 
-        const report = await Report.create({ Source, Description, user_id });
+        const report = await Report.create({ source, description, image, user_id });
 
         return res.status(200).json(report);
     } catch (error) {
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -23,37 +23,40 @@ userSchema.statics.signup = async function(email, password) {
     if (!email || !password) {
         throw Error('All fields must be filled')
     }
-    if (!validator.isEmail(email)) {
-        throw Error('Email not valid')
-    }
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-    if (!validator.matches(password, passwordRegex)) {
-    const errors = [];
-
-    if (!validator.matches(password, /^(?=.*[a-z])/)) {
-        errors.push('Password must contain at least one lowercase letter');
-    }
-
-    if (!validator.matches(password, /^(?=.*[A-Z])/)) {
-        errors.push('Password must contain at least one uppercase letter');
-    }
-
-    if (!validator.matches(password, /^(?=.*\d)/)) {
-        errors.push('Password must contain at least one number');
-    }
-
-    if (!validator.matches(password, /^(?=.*[@$!%*?&])/)) {
-        errors.push('Password must contain at least one special character');
-    }
-
-    throw new Error(errors.join('\n'));
-    }
-    
     const exists = await this.findOne({ email })
 
     if (exists) {
         throw Error('Email already in use')
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email not valid')
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        const errors = [];
+
+        if (password.length < 8) {
+            errors.push('Password must at least be 8 characters long.')
+        }
+
+        if (!validator.matches(password, /^(?=.*[a-z])/)) {
+            errors.push('Password must contain at least one lowercase letter');
+        }
+
+        if (!validator.matches(password, /^(?=.*[A-Z])/)) {
+            errors.push('Password must contain at least one uppercase letter');
+        }
+
+        if (!validator.matches(password, /^(?=.*\d)/)) {
+            errors.push('Password must contain at least one number');
+        }
+
+        if (!validator.matches(password, /^(?=.*\W)/)) {
+            errors.push('Password must contain at least one special character');
+        }
+
+        throw new Error(errors.join('\n'));
     }
 
     const salt = await bcrypt.genSalt(10)
