9565025d974d137e814874fdb1a132dcbf7d0f7d
danangeloj <danangelojulongbayan@gmail.com>
2023-06-08 18:53:45 +0800

    easter egg

--- client/components/AboutusComp/AboutusComp.js
+++ client/components/AboutusComp/AboutusComp.js
@@ -1,4 +1,5 @@
-import { View, Text, ImageBackground, Image, SafeAreaView } from 'react-native'
+import { View, Text, ImageBackground, Image, SafeAreaView, TouchableOpacity} from 'react-native'
+import { useState } from 'react'
 import React from 'react'
 import styles from './styles'
 import MapContainer from '../commons/mapContainer/Contain'
@@ -6,29 +7,48 @@ import Container from '../commons/Container/Contain'
 
 const AboutusComp = () => {
 
-  const members = [
+  const [members, setMembers] = useState([
     {
       image: require('../../assets/images/bats.png'),
       name: 'Kristian John Q. Baturiano',
       specialization: 'Data Science',
+      newImage: require('../../assets/images/baturiano_anime.jpg'),
+      tapCount: 0,
     },
     {
       image: require('../../assets/images/trystan.jpg'),
       name: 'Mickel Trystan Dones',
       specialization: 'Intelligent Systems',
+      newImage: require('../../assets/images/dones_anime.jpg'),
+      tapCount: 0,
     },
     {
       image: require('../../assets/images/Julongbayan.jpg'),
       name: 'Dan Angelo A. Julongbayan',
       specialization: 'Data Science',
+      newImage: require('../../assets/images/julongbayan_anime.jpg'),
+      tapCount: 0,
     },
     {
       image: require('../../assets/images/frank2.jpg'),
       name: 'Franklin T. Nazareno Jr',
       specialization: 'Technopreneurship',
+      newImage: require('../../assets/images/nazareno_anime.jpg'),
+      tapCount: 0,
     },
-  ];
+  ]);
+  const handleImageTap = (index) => {
+    const updatedMembers = [...members];
+    const member = updatedMembers[index];
   
+    if (member.tapCount >= 10) {
+      member.image = member.newImage;
+    }
+    member.tapCount = member.tapCount + 1;
+  
+    setMembers(updatedMembers);
+  };
+
   return (
     <Container>
         <ImageBackground 
@@ -42,9 +62,11 @@ const AboutusComp = () => {
             <View style={styles.row}>
               {members.map((member, index) => (
                 <View key={index} style={styles.memberimage}>
-                <Image 
-                source={member.image}
-                style={styles.imagesize}/>
+                  <TouchableOpacity onPress={() => handleImageTap(index)} activeOpacity={1}>
+                    <Image 
+                    source={member.image}
+                    style={styles.imagesize}/>
+                  </TouchableOpacity>
                 <Text style={styles.namefont}>{member.name}</Text>
                 <Text style={styles.namefont2}>{member.specialization}</Text>
                 </View>
